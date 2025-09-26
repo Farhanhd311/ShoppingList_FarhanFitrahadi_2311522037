4ccca71b94b1676107b12b26becd7bd09faf23b1
Farhanhd311 <farhan6839@gmail.com>
2025-09-26 08:33:45 +0700

    comit baruu lagi

--- app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.example.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Warna untuk light
+val RedLight = Color(0xFF009688)
+val RedDark = Color(0xFF009688)
+val RedOn = Color(0xFFFFFFFF)
+val RedBackground = Color(0xFFF5F5F5)
+val RedOnBackground = Color(0xFF1C1B1F)
+val RedSurface = Color(0xFFFFFFFF)
+val RedOnSurface = Color(0xFF1C1B1F)
+
+val RedSurfaceDark = Color(0xFF2B2930)
+val RedOnSurfaceDark = Color(0xFFE6E1E5)
+
+val RedSecondary = Color(0xFF023431)
+val RedSecondaryContainer = Color(0xFF174842)
+val RedOnSecondaryContainer = Color(0xFF4CAFA0)
+
+val RedPrimaryContainer = Color(0xFFD6F9FF)
+val RedOnPrimaryContainer = Color(0xFF0D443F)
+
+// Warna untuk dark mode
+val RedDarkBackground = Color(0xFF121212)
+val RedDarkOnBackground = Color(0xFFE6E1E5)
+val RedDarkPrimaryContainer = Color(0xFF8DEAE1)
+val RedDarkOnPrimaryContainer = Color(0xFF023431)
+val RedDarkSecondaryContainer = Color(0xFF174842)
+val RedDarkOnSecondaryContainer = Color(0xFF4CAFA0)
